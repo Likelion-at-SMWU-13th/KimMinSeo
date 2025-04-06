4f775f9221b986c008c718e5af1c16d05e736db6
danginseo <minseo91@sookmyung.ac.kr>
2025-04-06 23:27:57 +0900

    로그인 기능 추가

--- week04_hw/accounts/views.py
+++ week04_hw/accounts/views.py
@@ -13,4 +13,16 @@ def signup(request):
 
     return render(request, "signup.html", {"form":form})
 
+def login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password) # 사용자 인증
+        if user is not None:
+            auth_login(request, user)
+            return redirect("main")
+    
+    return render(request, "login.html")
+
 # Create your views here.
